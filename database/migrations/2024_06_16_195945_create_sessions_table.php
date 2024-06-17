75fe30d937c05f830dda403743501e0c7565472d
JPalacio <joelpalacio630@gmail.com>
2024-06-17 16:13:30 -0600

    create repository - first commimt

---
+++ database/migrations/2024_06_16_195945_create_sessions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
+
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('sessions', function (Blueprint $table) {
+//             $table->id();
+//             $table->timestamps();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('sessions');
+//     }
+// };
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
