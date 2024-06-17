a932b0ba1e8ad081923a4331dc74a0c4734c881c
JPalacio <joelpalacio630@gmail.com>
2024-06-17 17:01:42 -0600

    feat : Instalacion de Tailwindcss, postcss y autoprefixer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Devstagram - @yield('titulo')</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+</body>
+
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+Página Principal
+@endsection
+
+
+@section('contenido')
+Contenido de esta Página
+@endsection
