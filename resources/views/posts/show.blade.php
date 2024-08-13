7bdcca7b5e30a41260a8289e05c6769b72a38e6d
unknown <joelpalacio630@gmail.com>
2024-08-13 17:36:25 -0600

    feat : Añadiendo likes dinámicos sin salto de la página ni recarga para que actualice, por medio de livewire

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,39 +17,8 @@
           {{-- Componente de livewire --}}
           <livewire:like-post :post="$post" />
 
-          @if ($post->checkLike(auth()->user()))
-            <form action="{{ route('post.likes.destroy', $post) }}" method="POST">
-              @method('DELETE')
-              @csrf
-              <div class="my-4">
-                <button type="submit">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                  </svg>
-                </button>
-              </div>
-            </form>
-          @else
-            <form action="{{ route('post.likes.store', $post) }}" method="POST">
-              @csrf
-              <div class="my-4">
-                <button type="submit">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                  </svg>
-                </button>
-              </div>
-            </form>
-          @endif
         @endauth
 
-        <p class="font-bold">{{ $post->likes->count() }}
-          <span class="font-normal">Likes</span>
-        </p>
       </div>
 
       <div class="">
