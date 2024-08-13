675c73ab715f2c3f6eac743dae3bcf3724f93d36
unknown <joelpalacio630@gmail.com>
2024-08-13 17:48:19 -0600

    devstagram

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,125 +1,116 @@
 @extends('layouts.app')
 
 @section('titulo')
-  {{ $post->titulo }}
+{{ $post->titulo }}
 @endsection
 
 @section('contenido')
-  <div class="container mx-auto md:flex">
+<div class="container mx-auto md:flex">
 
     <div class="md:w-1/2">
-      <img class="rounded-xl" src=" {{ asset('uploads') . '/' . $post->imagen }}"
-        alt="Imagen del post {{ $post->titulo }}">
+        <img class="rounded-xl" src=" {{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
 
-      <div class="p-3 flex items-center gap-4">
+        <div class="p-3 flex items-center gap-4">
+            @auth
+
+            {{-- Componente de livewire --}}
+            <livewire:like-post :post="$post" />
+
+            @endauth
+
+        </div>
+
+        <div class="">
+            <p class="font-bold">{{ $post->user->username }}</p>
+            <p class="text-sm text-gray-500">
+                {{ $post->created_at->diffForHumans() }}
+            </p>
+
+            <p class="mt-5">
+                {{ $post->descripcion }}
+            </p>
+        </div>
+
         @auth
-
-          {{-- Componente de livewire --}}
-          <livewire:like-post :post="$post" />
-
-        @endauth
-
-      </div>
-
-      <div class="">
-        <p class="font-bold">{{ $post->user->username }}</p>
-        <p class="text-sm text-gray-500">
-          {{ $post->created_at->diffForHumans() }}
-        </p>
-
-        <p class="mt-5">
-          {{ $post->descripcion }}
-        </p>
-      </div>
-
-      @auth
         @if ($post->user_id === auth()->user()->id)
-          <form action="{{ route('posts.destroy', $post) }}" method="POST">
+        <form action="{{ route('posts.destroy', $post) }}" method="POST">
             @method('DELETE')
             @csrf
-            <input type="submit" value="Eliminar Publicación"
-              class="bg-red-500 hover:bg-red-800 p-2 rounded text-white font-bold mt-4 cursor-pointer" />
-          </form>
+            <input type="submit" value="Eliminar Publicación" class="bg-red-500 hover:bg-red-800 p-2 rounded text-white font-bold mt-4 cursor-pointer" />
+        </form>
         @endif
-      @endauth
+        @endauth
     </div>
 
     <div class="md:w-1/2 p-5">
 
-      <div class="shadow bg-white p-5 mb-5 rounded-xl">
+        <div class="shadow bg-white p-5 mb-5 rounded-xl">
 
-        {{-- Validar que el usuario esté autenticado para poder comentar e interactuar con la publicación --}}
-        @auth
-          <p class="text-xl font-bold text-center mb-4">
-            Agrega un Nuevo Comentario
-          </p>
+            {{-- Validar que el usuario esté autenticado para poder comentar e interactuar con la publicación --}}
+            @auth
+            <p class="text-xl font-bold text-center mb-4">
+                Agrega un Nuevo Comentario
+            </p>
 
-          @if (session('mensaje'))
-            <div
-              class="bg-green-600 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
-              {{ session('mensaje') }}
+            @if (session('mensaje'))
+            <div class="bg-green-600 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                {{ session('mensaje') }}
             </div>
-          @endif
+            @endif
 
-          <form
-            action="{{ route('comentarios.store', [
+            <form action="{{ route('comentarios.store', [
                 'post' => $post,
                 'user' => $user,
-            ]) }}"
-            method="POST" class="">
+            ]) }}" method="POST" class="">
 
-            @csrf
+                @csrf
 
-            <div class="mb-5 ">
-              <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
-                Añade tu comentario:
-              </label>
+                <div class="mb-5 ">
+                    <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Añade tu comentario:
+                    </label>
 
-              <textarea id="comentario" name="comentario" placeholder="Agrega un comentario"
-                class="
-                                border p-3 w-full   rounded-xl
-                                @error('comentario') border-red-500 bg-red-50 @enderror ">
-                                 {{ old('comentario') }}
-                            </textarea>
+                    <textarea id="comentario" name="comentario" placeholder="Agrega un comentario" class="
+                border p-3 w-full rounded-xl
+                @error('comentario') border-red-500 bg-red-50 @enderror ">
+                    {{ old('comentario') }}
+                    </textarea>
 
-              @error('comentario')
-                <p
-                  class = "bg-red-500 text-white my-2 rounded-xl text-sm p-2 text-center font-bold">
-                  {{ $message }}
-                </p>
-              @enderror
-            </div>
+                    @error('comentario')
+                    <p class="bg-red-500 text-white my-2 rounded-xl text-sm p-2 text-center font-bold">
+                        {{ $message }}
+                    </p>
+                    @enderror
+                </div>
 
-            <input type="submit" value="Comentar"
-              class="
+                <input type="submit" value="Comentar" class="
                         bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
                         uppercase font-bold w-full p-3 text-white rounded-xl" />
-          </form>
-        @endauth
+            </form>
+            @endauth
 
-        <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
-          @if ($post->comentarios->count())
-            @foreach ($post->comentarios->sortByDesc('created_at') as $comentario)
-              <div class="p-5 border-gray-300 border-b text-gray-800">
+            <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+                @if ($post->comentarios->count())
+                @foreach ($post->comentarios->sortByDesc('created_at') as $comentario)
+                <div class="p-5 border-gray-300 border-b text-gray-800">
 
-                <a href="{{ route('posts.index', $comentario->user) }}"
-                  class="font-bold text-xs">
-                  {{ $comentario->user->username }}
-                </a>
+                    <a href="{{ route('posts.index', $comentario->user) }}" class="font-bold text-xs">
+                        {{ $comentario->user->username }}
+                    </a>
 
-                <p class="">{{ $comentario->comentario }}</p>
+                    <p class="">{{ $comentario->comentario }}</p>
 
-                <p class="text-xs text-gray-500">
-                  {{ $comentario->created_at->diffForHumans() }}</p>
+                    <p class="text-xs text-gray-500">
+                        {{ $comentario->created_at->diffForHumans() }}</p>
 
-              </div>
-            @endforeach
-          @else
-            <p class="p-10 text-center text-gray-500 font-bold">Aún No Hay Comentarios</p>
-          @endif
+                </div>
+                @endforeach
+                @else
+                <p class="p-10 text-center text-gray-500 font-bold">Aún No Hay Comentarios</p>
+                @endif
 
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 @endsection
